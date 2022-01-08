d9a818cd2722ab5bb9f25c0080ebf0c131d34702
souradip <souradipsarkar77@gmail.com>
2022-01-08 22:59:51 +0530

    Commit message

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-function clock()
-{
-  var hours=document.getElementsById("hours");
-  var minutes=document.getElementsById("min");
-  var seconds=document.getElementsById("sec");
-  var tz=document.getElementsById("time5");
-
-  var time=new Date();
-  var a=time.getHours();
-  var b=time.getMinutes();
-  var c=time.getSeconds();
-
-  hours.innerHTML=a;
-  minutes.innerHTML=b;
-  seconds.innerHTML=c;
-
-
-}
-
-setInterval(clock,1000);
+function clock() {
+    var hours = document.getElementById("hours");
+    var minutes = document.getElementById("minutes");
+    var seconds = document.getElementById("seconds");
+    var tz = document.getElementById("time5");
+    var time = new Date();
+    var a = time.getHours();
+    var b = time.getMinutes();
+    var c = time.getSeconds();
+    hours.innerHTML = a;
+    minutes.innerHTML = b;
+    seconds.innerHTML = c;
+  }
+  setInterval(clock, 1000);
